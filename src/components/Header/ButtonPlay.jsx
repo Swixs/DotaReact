a31c28fc26dedb83f165f23ea33429359c2432c8
Nikita <Nekit4spice@gmail.com>
2023-11-10 13:13:49 +0100

    New commit complete header line

---
+++ src/components/Header/ButtonPlay.jsx
@@ -0,0 +1,18 @@
+import classLine from './HeaderLine.module.css'
+
+const ButtonPlay = () => {
+
+    const BtnPlay = () => {
+        window.location.href = 'https://store.steampowered.com/app/570/Dota_2/'
+    }
+
+    return (
+        <div>
+            <button className={classLine.btnPlay} onClick={BtnPlay} > <img className={classLine.iconSteam}
+                src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/steam_icon.svg" 
+                alt="SteamIcon"/> Play for free </button>
+        </div>
+    )
+}
+
+export default ButtonPlay
--- src/components/Header/HeaderLine.jsx
+++ src/components/Header/HeaderLine.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import classLine from './HeaderLine.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown, faGlobe } from '@fortawesome/free-solid-svg-icons';
+import ButtonPlay from "./ButtonPlay";
+
+
 
 const HeaderLine = () => {
+
+    const linkLogin = () => {
+        window.location.href = 'https://steamcommunity.com/oauth/login?client_id=9B2C1229&response_type=token&state=home';
+    }
+
     return (
         <div className={classLine.items}>
             <button className={classLine.logoItem}>
@@ -27,6 +35,25 @@ const HeaderLine = () => {
             <button className={classLine.button}>Heroes</button>
             <button className={classLine.button}>News</button>
             <button className={classLine.button}>Esports</button>
+
+            <div className={classLine.rightBtns}>
+                <button className={classLine.btnLogin} onClick={linkLogin}>login</button>
+                <div className={classLine.dropdownTranslator}>
+                    <button className={classLine.dropButton}>
+                        <FontAwesomeIcon icon={faGlobe} style={{ color: "#ffffff", }} className={classLine.iconPlanet} />
+                        Select Language
+                        <FontAwesomeIcon icon={faCaretDown} style={{ "--fa-primary-color": "#ffffff", "--fa-secondary-color": "#fafafa", }} className={classLine.iconLanguage} />
+                    </button>
+                    <div className={classLine.dropdownCont}>
+                        <a href="#">English</a>
+                        <a href="#">Italian</a>
+                        <a href="#">Polski</a>
+                        <a href="#">Deutsch</a>
+                    </div>
+                </div>
+            </div>
+
+            <ButtonPlay />
         </div>
     );
 };
