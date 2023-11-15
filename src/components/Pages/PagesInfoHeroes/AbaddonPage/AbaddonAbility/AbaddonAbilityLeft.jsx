f9dc85850d97197a77d0c3163ec49382ed116ff3
Nikita <Nekit4spice@gmail.com>
2023-11-15 22:33:43 +0100

    New commit fix

--- src/components/Pages/PagesInfoHeroes/AbaddonPage/AbaddonAbility/AbaddonAbilityLeft.jsx
+++ src/components/Pages/PagesInfoHeroes/AbaddonPage/AbaddonAbility/AbaddonAbilityLeft.jsx
@@ -1,17 +1,22 @@
 import classAbaddonPage from '../AbaddonPage.module.css';
 
 const AbaddoneAbilityLeft = () => {
+
+    function click(){
+        console.log(1)
+    }
+
     return (
         <div>
 
-            <div className={classAbaddonPage.videoContainer}>
+            <div className={classAbaddonPage.videoContainerAbility}>
                 <video className={classAbaddonPage.videoAbility} src="https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/abilities/abaddon/abaddon_death_coil.webm"
                     autoPlay muted loop>
                 </video>
             </div>
 
             <div>
-                <button>
+                <button onClick={click} className={classAbaddonPage.btn}>
                     <img className={classAbaddonPage.spell} src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/abilities/abaddon_death_coil.png" alt="firstSpell" />
                 </button>
             </div>
