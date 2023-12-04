0f408a8506e8bc1f3a0cb6a4503e4704a869ddf9
Nikita <Nekit4spice@gmail.com>
2023-12-04 20:55:49 +0100

    Complete

--- src/components/Pages/PagesInfoHeroes/AbaddonPage/FooterButtons/FooterButtonsCenter.jsx
+++ src/components/Pages/PagesInfoHeroes/AbaddonPage/FooterButtons/FooterButtonsCenter.jsx
@@ -2,8 +2,20 @@ import classAbaddonPage from '../AbaddonPage.module.css';
 
 const FooterButtonsCenter = () => {
    return (
-      <div>
-         <button className={classAbaddonPage.buttonCenter}>ALL HEROES</button>
+      <div className={classAbaddonPage.btnNet}>
+         <button className={classAbaddonPage.buttonCenter}>
+            <div className={classAbaddonPage.lineDisplay}>
+               <div className={classAbaddonPage.btnCenterCube}></div>
+               <div className={classAbaddonPage.btnCenterCube}></div>
+               <div className={classAbaddonPage.btnCenterCube}></div>
+            </div>
+            <div className={classAbaddonPage.lineDisplay}>
+               <div className={classAbaddonPage.btnCenterCube}></div>
+               <div className={classAbaddonPage.btnCenterCube}></div>
+               <div className={classAbaddonPage.btnCenterCube}></div>
+            </div>
+            <div className={classAbaddonPage.textCenterButton}>ALL HEROES</div>
+         </button>
       </div>
    );
 };
--- src/components/Pages/PagesInfoHeroes/AbaddonPage/FooterButtons/FooterButtonsLeft.jsx
+++ src/components/Pages/PagesInfoHeroes/AbaddonPage/FooterButtons/FooterButtonsLeft.jsx
@@ -4,6 +4,19 @@ const FooterButtonsLeft = () => {
    return (
       <div>
          <button className={classAbaddonPage.buttonLeft}>
+            <div className={classAbaddonPage.buttonTextLeft}>
+               <div>PREVIOUS HERO</div>
+               <div className={classAbaddonPage.titleButton}>ZEUS</div>
+               <div className={classAbaddonPage.subTitleButton}>
+                  <img
+                     className={classAbaddonPage.buttonTextAtr}
+                     src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_intelligence.png"
+                     alt="hero Atr"
+                  />
+                  RANGED
+               </div>
+            </div>
+
             <div className={classAbaddonPage.buttonPictureLeft}>
                <img
                   src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/crops/zuus.png"
