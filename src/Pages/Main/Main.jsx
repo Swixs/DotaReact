ef69a14260cdb4b7ef70fe270eb270e140831348
Nikita <Nekit4spice@gmail.com>
2024-01-02 15:22:29 +0100

    complete news main

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import classMain from './Main.module.css';
 import Video from './topMain/Video/video';
 import TopMain from './topMain/topMain';
+import NewsMain from './newsMain/newsMain';
 
 const Main = () => {
    return (
       <div className={classMain.mainContent}>
          <Video />
          <TopMain />
+         <NewsMain />
       </div>
    );
 };
---
+++ src/Pages/Main/newsMain/newsMain.jsx
@@ -0,0 +1,43 @@
+import classMain from './../Main.module.css';
+
+const NewsMain = () => {
+   return (
+      <div className={classMain.backgroundNews}>
+         <button className={classMain.firstBtnNews}>
+            <div className={classMain.timeBtnNews}>15 DECEMBER 2023 Y.</div>
+            <div className={classMain.titleBtnNews}>
+               The Frostivus 2023 Update is Here
+            </div>
+            <div className={classMain.textBtnNews}>
+               Another Frostivus is upon us, and that means King Kringle has
+               awakened from his hibernation under the wine-dark Blood Seas
+               surrounding Frostivus Castle; sent his legions of...
+            </div>
+         </button>
+         <button className={classMain.secondBtnNews}>
+            <div className={classMain.timeBtnNews}>14 DECEMBER 2023 Y.</div>
+            <div className={classMain.titleBtnNews}>
+               The Dark Night Before Frostivus
+            </div>
+            <div className={classMain.textBtnNews}>
+               'Twas Frostivus Eve, and all 'cross the map, The neutrals were
+               quiet, their camps left unstacked. We'd gorged on a feast of
+               bluehorn and spiced creep, And had just settled in for...
+            </div>
+         </button>
+         <button className={classMain.thirdBtnNews}>
+            <div className={classMain.timeBtnNews}>21 NOVEMBER 2023 Y.</div>
+            <div className={classMain.titleBtnNews}>
+               Gameplay Update 7.34e and What’s Up Next
+            </div>
+            <div className={classMain.textBtnNews}>
+               After nearly a year of coordination and production, the
+               multi-week, arena-sized monster that is The International 2023
+               has come to an exciting close.
+            </div>
+         </button>
+      </div>
+   );
+};
+
+export default NewsMain;
